b2c4f17016327ab8cfabc253a1f3e8a445d7d624
kristoffee <34727428+kristoffee@users.noreply.github.com>
2023-10-03 13:47:15 +1100

    create vpc module

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "blog_sg" {
   version = "5.1.0"
   
   name    = "blog"
-  vpc_id  = modeule.vpc.public_subnets[0]
+  vpc_id  = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
